863a46b74233637cd0b3079f984abacb862826e8
guidogerb <guidogerb1966@outlook.com>
2023-04-04 09:57:00 -0600

    added chatGPT dialogs

---
+++ implementation/ids/src/test/java/com/bpm/BitWiseTests.java
@@ -0,0 +1,12 @@
+package com.bpm;
+
+import org.junit.Test;
+import java.util.List;
+import static org.junit.Assert.*;
+
+public class BitWiseTests {
+    @Test
+    public void isTrue() {
+        assertTrue(true);
+    }
+}
